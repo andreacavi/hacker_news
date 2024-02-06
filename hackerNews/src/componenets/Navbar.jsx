e70cb142e2299ed0f33f392be4ecbe01278ab85e
Niloufar Asghari <nilou.asghari@gmail.com>
2024-02-06 16:24:35 +0100

    creating the first version of navbar

---
+++ hackerNews/src/componenets/Navbar.jsx
@@ -0,0 +1,10 @@
+import "../App.css";
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navBar">
+        <div className="logo">Hacker News</div>
+      </nav>
+    </div>
+  );
+}
