1ac62451c37a50e6a732911c6e3fb38264c2f370
Niloufar Asghari <nilou.asghari@gmail.com>
2024-02-07 10:13:31 +0100

    fetching more data

--- hackerNews/src/App.jsx
+++ hackerNews/src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
+
 import "./App.css";
 import Navbar from "./componenets/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [data, setData] = useState([]);
+  const url = "https://hn.algolia.com/api/v1/search?query=react";
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setData(data.hits);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    fetchData();
+    console.log(data.hits);
+  }, []);
 
   return (
     <>
       <Navbar />
+      <div className="container">
+        <ul class="newsContainer">
+          {data.map((item) => (
+            <li key={item.objectID}>
+              <div key={item.objectID}>by {item.author}</div>
+              <a href={`${item.url}`}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
